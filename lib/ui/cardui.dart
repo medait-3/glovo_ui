99110fdbeea9ff4cbb6270b4c03cb098dd0e549e
meait-3 <tchikh3@@gmail.com>
2023-07-11 13:42:49 +0100

    big card

--- lib/ui/cardui.dart
+++ lib/ui/cardui.dart
@@ -25,14 +25,11 @@ class CardUi extends StatelessWidget {
           Stack(
             clipBehavior: Clip.none, // <-- add this
             children: [
-              Container(
-                alignment: Alignment.center,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(18.0),
-                  child: Image.network(
-                    'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                    fit: BoxFit.cover,
-                  ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(18.0),
+                child: Image.network(
+                  'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                  fit: BoxFit.cover,
                 ),
               ),
               Padding(
